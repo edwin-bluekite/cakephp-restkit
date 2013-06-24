2473e5894bf1a7b05b468f6f09c03d9a8242ee0d
bravo-kernel <dev@bravo-kernel.com>
2013-06-24 13:03:33 +0200

    Moving HAL xml rendering to HalHelper

--- View/Helper/HalHelper.php
+++ View/Helper/HalHelper.php
@@ -7,8 +7,19 @@ App::uses('AppHelper', 'View/Helper');
  */
 class HalHelper extends AppHelper {
 
+	/**
+	 * Helpers collection
+	 *
+	 * @var HelperCollection
+	 */
 	public $helpers = array('RestKit.RestKit');
 
+	/**
+	 * makeJsonPlural() generates a HAL-formatted array from (collection) $data ready for json encoding
+	 *
+	 * @param array $data
+	 * @return array
+	 */
 	public function makeJsonPlural($data) {
 
 		$out = array();
@@ -42,10 +53,10 @@ class HalHelper extends AppHelper {
 	}
 
 	/**
-	 * _makeJsonSingular() generates a HAL-formatted (single resource) array from $data before returning it json_encoded
+	 * makeJsonSingular() generates a HAL-formatted array from (single resource) $data ready for json encoding
 	 *
-	 * @param type $data
-	 * @return type
+	 * @param array $data
+	 * @return array
 	 */
 	public function makeJsonSingular($data) {
 
@@ -74,8 +85,8 @@ class HalHelper extends AppHelper {
 	/**
 	 * getJsonHalSelf() ....
 	 *
-	 * @param type $modelName
-	 * @param type $id
+	 * @param string $modelName
+	 * @param string $id
 	 * @return array
 	 */
 	protected function _getJsonHalSelf($modelName, $id = null) {
@@ -93,8 +104,8 @@ class HalHelper extends AppHelper {
 	/**
 	 * getJsonHal ....
 	 *
-	 * @param type $modelName
-	 * @param type $id
+	 * @param string $modelName
+	 * @param string $id
 	 * @return array
 	 */
 	protected function _getJsonHal($modelName, $id = null) {
@@ -104,4 +115,109 @@ class HalHelper extends AppHelper {
 		}
 		return array('href' => $url);
 	}
+
+	/**
+	 * makeXmlPlural() generates a HAL-formatted array from (collection) $data ready for xml encoding
+	 *
+	 * @param type $data
+	 * @return array
+	 */
+	public function makeXmlPlural($data) {
+
+		$out = array();
+		$out['resource'] = $this->_getXmlHal($this->RestKit->modelClass);
+		$out['resource']['resource'] = array();
+
+		foreach ($data as $index => $record) {
+			$temp = array();
+
+			// self-link first
+			$temp += $this->_getXmlHal($this->RestKit->modelClass, $record[$this->RestKit->modelClass]['id']);
+
+			// next process record-fields
+			$temp['link'] = array();
+			foreach ($record[$this->RestKit->modelClass] as $fieldName => $value) {
+
+				if (!$this->RestKit->isExcludedField($fieldName)) {
+					if (preg_match('/(.+)_id$/', $fieldName, $matches)) {  // everything before the last '_id' in the string will be in $matches[1], e.g. country
+						if ($this->RestKit->isForeignField($fieldName)) {
+							$temp += array($fieldName => $value);
+						} else {
+							array_push($temp['link'], $this->_getXmlHal($matches[1], $value));
+						}
+					} else {
+						$temp += array($fieldName => $value);
+					}
+				}
+			}
+			// prevent empty <link/> appearing in the XML when no _id fields were present
+
+			if (count($temp['link']) == 0) {
+				unset($temp['link']);
+			}
+			array_push($out['resource']['resource'], $temp);
+		}
+		return $out;
+	}
+
+	/**
+	 * makeXmlPlural() generates a HAL-formatted array from (single resource) $data ready for xml encoding
+	 *
+	 * @param array $data
+	 * @return array
+	 */
+	public function makeXmlSingular($data) {
+
+		// self-link first
+		$out = array();
+		$out = $this->_getXmlHal($this->RestKit->modelClass, $data[$this->RestKit->modelClass]['id']);
+
+		// record-fields next
+		$out['link'] = array();
+		foreach ($data[$this->RestKit->modelClass] as $fieldName => $value) {
+
+			if (!$this->RestKit->isExcludedField($fieldName)) {
+				if (preg_match('/(.+)_id$/', $fieldName, $matches)) {  // everything before the last '_id' in the string will be in $matches[1], e.g. country
+					if ($this->RestKit->isForeignField($fieldName)) {
+						$out += array($fieldName => $value);
+					} else {
+						array_push($out['link'], $this->_getXmlHal($matches[1], $value));
+					}
+				} else {
+					$out += array($fieldName => $value);
+				}
+			}
+		}
+		return array('resource' => $out);
+	}
+
+	/**
+	 * _getXmlHal ....
+	 *
+	 * @param type $modelName
+	 * @param type $id
+	 * @return array
+	 */
+	protected function _getXmlHal($modelName, $id = null) {
+
+		$out = array();
+
+		// set up rel first (only if we have an id to prevent rel popping up for the root)
+		if ($id) {
+			$rel = Inflector::tableize($modelName);
+			$rel = Inflector::singularize($rel);
+			$out['@rel'] = $rel;
+		}
+
+		// set up the url next
+		$url = '/' . Inflector::tableize($modelName);
+		if (isset($id)) {
+			$url .= '/' . $id;
+		}
+		$out['@href'] = $url;
+
+		// return data
+		return $out;
+	}
+
 }
--- View/RestKitXmlView.php
+++ View/RestKitXmlView.php
@@ -22,127 +22,20 @@ class RestKitXmlView extends RestKitView {
 	}
 
 	/**
-	 * _serializeHal() is used to pass Cake find() data to the HAL array-formatters before
-	 * returning it as XML.
+	 * _serializeHal() uses the RestKit.HalHelper to format find() data into
+	 * HAL format before returning it as XML
 	 *
-	 * @param type $data
-	 * @return string
+	 * @param array $data
 	 */
 	protected function _serializeHal($data) {
+		$helper = $this->Helpers->load('RestKit.Hal');
 		if ($this->plural) {
-			return Xml::fromArray($this->_makeHalPlural($data))->asXML();
+			return Xml::fromArray($helper->makeXmlPlural($data))->asXML();
 		}
-		return Xml::fromArray($this->_makeHalSingular($data))->asXML();
+		return Xml::fromArray($helper->makeXmlSingular($data))->asXML();
 	}
 
 
-
-
-
-
-	/**
-	 * _makeHalPlural() generates a HAL-formatted (collection) array from $data before returning it as XML
-	 *
-	 * @param type $data
-	 * @return type
-	 */
-	protected function _makeHalPlural($data) {
-
-		$out = array();
-		$out['resource'] = $this->_getXmlHal($this->modelClass);
-		$out['resource']['resource'] = array();
-
-		foreach ($data as $index => $record) {
-			$temp = array();
-
-			// self-link first
-			$temp += $this->_getXmlHal($this->modelClass, $record[$this->modelClass]['id']);
-
-			// next process record-fields
-			$temp['link'] = array();
-			foreach ($record[$this->modelClass] as $fieldName => $value) {
-
-				if (!$this->_isExcluded($fieldName)) {
-					if (preg_match('/(.+)_id$/', $fieldName, $matches)) {  // everything before the last '_id' in the string will be in $matches[1], e.g. country
-						if ($this->_isForeign($fieldName)) {
-							$temp += array($fieldName => $value);
-						} else {
-							array_push($temp['link'], $this->_getXmlHal($matches[1], $value));
-						}
-					} else {
-						$temp += array($fieldName => $value);
-					}
-				}
-			}
-			// prevent empty <link/> appearing in the XML when no _id fields were present
-
-			if (count($temp['link']) == 0) {
-				unset($temp['link']);
-			}
-			array_push($out['resource']['resource'], $temp);
-		}
-		return $out;
-	}
-
-	/**
-	 * _makeHalSingular() generates a HAL-formatted (single resource) array from $data before returning it as XML
-	 *
-	 * @param type $data
-	 * @return type
-	 */
-	protected function _makeHalSingular($data) {
-
-		// self-link first
-		$out = array();
-		$out = $this->_getXmlHal($this->modelClass, $data[$this->modelClass]['id']);
-
-		// record-fields next
-		$out['link'] = array();
-		foreach ($data[$this->modelClass] as $fieldName => $value) {
-
-			if (!$this->_isExcluded($fieldName)) {
-				if (preg_match('/(.+)_id$/', $fieldName, $matches)) {  // everything before the last '_id' in the string will be in $matches[1], e.g. country
-					if ($this->_isForeign($fieldName)) {
-						$out += array($fieldName => $value);
-					} else {
-						array_push($out['link'], $this->_getXmlHal($matches[1], $value));
-					}
-				} else {
-					$out += array($fieldName => $value);
-				}
-			}
-		}
-		return array('resource' => $out);
-	}
-
-	/**
-	 * _getXmlHal ....
-	 *
-	 * @param type $modelName
-	 * @param type $id
-	 * @return array
-	 */
-	protected function _getXmlHal($modelName, $id = null) {
-
-		$out = array();
-
-		// set up rel first (only if we have an id to prevent rel popping up for the root)
-		if ($id) {
-			$rel = Inflector::tableize($modelName);
-			$rel = Inflector::singularize($rel);
-			$out['@rel'] = $rel;
-		}
-
-		// set up the url next
-		$url = '/' . Inflector::tableize($modelName);
-		if (isset($id)) {
-			$url .= '/' . $id;
-		}
-		$out['@href'] = $url;
-
-		// return data
-		return $out;
-	}
 
 	/**
 	 * _serializeException() generates an error.vnd response as described at
